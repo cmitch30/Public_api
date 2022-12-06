bc2039fdc5564424761fd3a2996bb6dbcb0986da
clark <clark_mitchelljr@hotmail.com>
2022-12-06 09:25:30 -0700

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let employees = [];
 const url = `https://randomuser.me/api/?results=12&inc=name, picture,
 email, location, phone, dob &noinfo &nat=US`;
-modal()
+
 const searchContainer = document.querySelector(".search-container");
 const searchInput = document.createElement("input");
 const submitInput = document.createElement("input");
@@ -31,6 +31,7 @@ fetch(url)
 
 function directory(employee) {
   employees = employee;
+  // console.log(employees)
   let galleryHTML = ``;
   employees.forEach((employee) => {
     galleryHTML += `<div class="card">
@@ -45,21 +46,24 @@ function directory(employee) {
         </div>`;
   });
   gallery.insertAdjacentHTML("beforeend", galleryHTML);
-}
-
-function modal() {
-       let modalHTML = `<div class="modal-container" style="display: none" data-index="">
+ 
+  gallery.addEventListener('click', (e) => {
+    let modalHTML = ``;
+    for (let i = 0; i < employee.length; i++) {
+      const person = employee[i]
+      console.log(person)
+        modalHTML += `<div class="modal-container" style="display: none">
         <div class="modal">
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
             <div class="modal-info-container">
-                <img id="img" class="modal-img" src="" alt="profile picture">
-                <h3 id="name" class="modal-name cap"></h3>
-                <p id="email" class="modal-text"></p>
-                <p id="city" class="modal-text cap"></p>
+                <img id="img" class="modal-img" src="${person.picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
+                <p id="email" class="modal-text">${person.email}</p>
+                <p id="city" class="modal-text cap">${person.location.city}</p>
                 <hr>
-                <p id="cell" class="modal-text"></p>
-                <p id="address" class="modal-text"></p>
-                <p id="birthday" class="modal-text">Birthday: </p>
+                <p id="cell" class="modal-text">${person.phone}</p>
+                <p id="address" class="modal-text">${person.location.street.number} ${person.location.street.name}</p>
+                <p id="birthday" class="modal-text">Birthday:${person.dob.date} </p>
             </div>
         </div>
         <div class="modal-btn-container">
@@ -67,8 +71,50 @@ function modal() {
                 <button type="button" id="modal-next" class="modal-next btn">Next</button>
         </div>
     </div>`;
-    gallery.insertAdjacentHTML("afterend", modalHTML);
+      }
+      gallery.insertAdjacentHTML("afterend", modalHTML);
+    })
+  }
+
+  const modalContainer = document.querySelector('.modal-container')
+
+function openModal(index) {
+modalContainer.addEventListener('click', (e) => {
+  modalContainer[index].style.display = 'block'
+})
 }
+
+function closeModal() {
+  const button = document.getElementById('modal-close-btn')
+  button.addEventListener('click', (e) => {
+    modalContainer.style.display = 'none'
+  })
+}
+
+openModal();
+closeModal();
+// function modal() {
+//        let modalHTML = `<div class="modal-container" style="display: none" data-index="">
+//         <div class="modal">
+//             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+//             <div class="modal-info-container">
+//                 <img id="img" class="modal-img" src="" alt="profile picture">
+//                 <h3 id="name" class="modal-name cap"></h3>
+//                 <p id="email" class="modal-text"></p>
+//                 <p id="city" class="modal-text cap"></p>
+//                 <hr>
+//                 <p id="cell" class="modal-text"></p>
+//                 <p id="address" class="modal-text"></p>
+//                 <p id="birthday" class="modal-text">Birthday: </p>
+//             </div>
+//         </div>
+//         <div class="modal-btn-container">
+//                 <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+//                 <button type="button" id="modal-next" class="modal-next btn">Next</button>
+//         </div>
+//     </div>`;
+//     gallery.insertAdjacentHTML("afterend", modalHTML);
+// }
 
 inputField.addEventListener("keyup", (e) => {
   let currentValue = e.target.value.toLowerCase();
@@ -84,6 +130,3 @@ inputField.addEventListener("keyup", (e) => {
 
 
 
-gallery.addEventListener('click', (e) => {
-
-})
